a0f863cc967cde6d2ca53b5cfd9088abacadaa02
Morgan Helton <mhelton@gmail.com>
2025-11-01 15:37:11 -0500

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Command line interface for wolweb";
+
+  # Nixpkgs / NixOS version to use.
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+
+      # to work with older version of flakes
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+
+      # Generate a user-friendly version number.
+      version = builtins.substring 0 8 lastModifiedDate;
+
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+    in
+    {
+
+      # Provide some binary packages for selected system types.
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          wolweb-cli = pkgs.buildGoModule {
+            pname = "wolweb-cli";
+            inherit version;
+            src = ./.;
+
+            vendorHash = "sha256-8q6cXf3khxjIInu2/bNQHl0xJRl/Sk43Qy0V6P8UCbY=";
+          };
+        });
+
+      # The default package for 'nix build'. This makes sense if the
+      # flake provides only one package or there is a clear "main"
+      # package.
+      defaultPackage = forAllSystems (system: self.packages.${system}.wolweb-cli);
+    };
+}
